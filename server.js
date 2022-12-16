5d97c0c8858ef5d55bc75e0d6cdfa2e1e8b6dc8a
Fatemeh Rahimi <fa.rahimi5475@gmail.com>
2022-12-16 18:40:29 +0000

    create server

---
+++ server.js
@@ -0,0 +1,42 @@
+// server.js
+// This is where your node app starts
+//load the 'express' module which makes writing webservers easy
+const express = require("express");
+const app = express();
+//load the quotes JSON
+const quotes = require("./quotes.json");
+// Now register handlers for some routes:
+//   /                  - Return some helpful welcome info (text)
+//   /quotes            - Should return all quotes (json)
+//   /quotes/random     - Should return ONE quote (json)
+app.get("/", function (request, response) {
+
+  response.send("<h1>Hello there! welcome to my quote server Api</h1>");
+});
+
+//START OF YOUR CODE...
+app.get("/quotes", function (request, response) {
+  response.send(quotes);
+});
+
+app.get("/quotes/random", function (request, response) {
+  response.send(pickFromArray(quotes));
+});
+
+//...END OF YOUR CODE
+
+//You can use this function to pick one element at random from a given array
+//example: pickFromArray([1,2,3,4]), or
+//example: pickFromArray(myContactsArray)
+//
+function pickFromArray(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+//Start our server so that it listens for HTTP requests!
+
+const listener = app.listen(3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
+//process.env.PORT
